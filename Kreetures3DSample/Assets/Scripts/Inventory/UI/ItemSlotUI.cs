391815dc3bef63dc69a9e2603ab2543d48e3d620
zachoryb98 <zbehrensmeyer@yahoo.com>
2023-10-29 16:41:39 -0500

    InventoryUI Part 2

---
+++ Kreetures3DSample/Assets/Scripts/Inventory/UI/ItemSlotUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlotUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI countText;
+
+    public TextMeshProUGUI NameText => nameText;
+    public TextMeshProUGUI CountText => countText;
+
+    public void SetData(ItemSlot itemSlot)
+    {
+        nameText.text = itemSlot.Item.Name;
+        countText.text = $"X {itemSlot.Count}";
+    }
+}
