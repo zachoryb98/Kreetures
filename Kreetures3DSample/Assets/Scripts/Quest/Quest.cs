eeb5545eac0730c5b97905e5657622ca92c3e7c0
Zach Behrensmeyer <zbehrensmeyer@yahoo.com>
2023-12-27 14:40:43 -0600

    Quest updates
    Enable/Disable things based on quests

--- Kreetures3DSample/Assets/Scripts/Quest/Quest.cs
+++ Kreetures3DSample/Assets/Scripts/Quest/Quest.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Quest
 {
     public QuestBase Base { get; private set; }
@@ -17,6 +18,9 @@ public class Quest
         Status = QuestStatus.Started;
 
         yield return DialogManager.Instance.ShowDialog(Base.StartDialogue);
+
+        var questList = QuestList.GetQuestList();
+        questList.AddQuest(this);
     }
 
     public IEnumerator CompleteQuest(Transform player)
@@ -38,6 +42,9 @@ public class Quest
             string playerName = player.GetComponent<PlayerController>().name;
             yield return DialogManager.Instance.ShowDialogText($"{playerName} received {Base.RewardItem.ItemName}");
         }
+
+        var questList = QuestList.GetQuestList();
+        questList.AddQuest(this);
     }
 
     public bool CanBeCompleted()
